b462eca2240ea5faa2a39f90b25069c808b8d1e2
Julienraptor01 <julienraptor01@gmail.com>
2024-06-26 16:05:13 +0200

    change the version string

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace AncientMonkey;
 public static class ModHelperData
 {
 	public const string WorksOnVersion = "42.0";
-	public const string Version = "4.1.0";
+	public const string Version = "4.1.0-NoCustom";
 	public const string Name = "AncientMonkey";
 
 	public const string Description = "Add the Ancient Monkey to Bloons TD6";
